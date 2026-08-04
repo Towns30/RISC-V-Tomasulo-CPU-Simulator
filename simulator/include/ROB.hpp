ecbb9459fd5f60804128c4d4517b50372c698f4e
Towns <tangzheyu@sjtu.edu.cn>
2026-08-04 20:20:31 +0800

    feat ROB

---
+++ simulator/include/ROB.hpp
@@ -0,0 +1,93 @@
+#ifndef ROB_HPP
+#define ROB_HPP
+
+#include "Sign.hpp"
+
+enum class WriteMemState
+{
+  NoWrite,
+  NotDone
+};
+
+struct ROBInfo
+{
+  ROBType rob_type = ROBType::WriteReg;
+  bool rd_valid_ = false;
+  uint8_t rd_ = 0;
+  bool rd_value_valid_ = false;
+  uint32_t rd_value_ = 0;
+  bool addr_valid_ = false;
+  uint32_t addr_ = 0;
+  bool addr_value_valid_ = false;
+  uint32_t addr_value_ = 0;
+  bool write_mem_state_valid_ = false;
+  WriteMemState write_mem_state_ = WriteMemState::NoWrite;
+  bool predict_pc_valid_ = false;
+  uint32_t predict_pc_ = 0;
+  bool real_pc_valid_ = false;
+  uint32_t real_pc_ = 0;
+  bool ready_ = false;
+
+  auto operator<=>(const ROBInfo &) const = default;
+};
+
+struct ROBState
+{
+  ROBInfo rob_infos_[16]{};
+  uint8_t head_ = 0;
+  uint8_t tail_ = 0;
+
+  auto operator<=>(const ROBState &) const = default;
+};
+
+struct ROBQueryResult
+{
+  bool valid_ = false;
+  uint32_t value_ = 0;
+
+  auto operator<=>(const ROBQueryResult &) const = default;
+};
+
+struct ROBQueryIDResult
+{
+  bool valid_ = false;
+  uint32_t rob_id_ = 0;
+
+  auto operator<=>(const ROBQueryIDResult &) const = default;
+};
+
+class ROB
+{
+public:
+  ROB(const IssueToROBSign &issue_input_sign,
+      const LSQToROBSign &lsq_input_sign,
+      const CDBToRS_LSQ_ROB_IssueSign &cdb_input_sign,
+      const MemToROBSign &memory_input_sign,
+      ROBToIssue_RAT_LSQ_RS_Decoder_CDB_Memory_ALUFlushSign &flush_output_sign,
+      ROBToFetchFlushSign &fetch_flush_output_sign,
+      ROBToLSQSign &lsq_output_sign, ROBToRATSign &rat_output_sign,
+      ROBToRegisterSign &register_output_sign);
+
+  bool Ready();
+  ROBQueryIDResult NextId();
+  ROBQueryResult Query(uint8_t rob_id);
+
+  void UpdateCurrent();
+  void Execute();
+  void UpdateNext();
+
+private:
+  const IssueToROBSign &issue_input_sign_;
+  const LSQToROBSign &lsq_input_sign_;
+  const CDBToRS_LSQ_ROB_IssueSign &cdb_input_sign_;
+  const MemToROBSign &memory_input_sign_;
+  ROBToIssue_RAT_LSQ_RS_Decoder_CDB_Memory_ALUFlushSign &flush_output_sign_;
+  ROBToFetchFlushSign &fetch_flush_output_sign_;
+  ROBToLSQSign &lsq_output_sign_;
+  ROBToRATSign &rat_output_sign_;
+  ROBToRegisterSign &register_output_sign_;
+  ROBState current_state_;
+  ROBState next_state_;
+};
+
+#endif
---
+++ simulator/src/ROB.cpp
@@ -0,0 +1,223 @@
+#include "ROB.hpp"
+
+ROB::ROB(
+    const IssueToROBSign &issue_input_sign, const LSQToROBSign &lsq_input_sign,
+    const CDBToRS_LSQ_ROB_IssueSign &cdb_input_sign,
+    const MemToROBSign &memory_input_sign,
+    ROBToIssue_RAT_LSQ_RS_Decoder_CDB_Memory_ALUFlushSign &flush_output_sign,
+    ROBToFetchFlushSign &fetch_flush_output_sign, ROBToLSQSign &lsq_output_sign,
+    ROBToRATSign &rat_output_sign, ROBToRegisterSign &register_output_sign)
+    : issue_input_sign_(issue_input_sign), lsq_input_sign_(lsq_input_sign),
+      cdb_input_sign_(cdb_input_sign), memory_input_sign_(memory_input_sign),
+      flush_output_sign_(flush_output_sign),
+      fetch_flush_output_sign_(fetch_flush_output_sign),
+      lsq_output_sign_(lsq_output_sign), rat_output_sign_(rat_output_sign),
+      register_output_sign_(register_output_sign)
+{
+}
+
+bool ROB::Ready()
+{
+  if ((current_state_.tail_ + 1) % 16 == current_state_.head_)
+  {
+    return false;
+  }
+  return true;
+}
+
+ROBQueryIDResult ROB::NextId()
+{
+  if ((current_state_.tail_ + 1) % 16 == current_state_.head_)
+  {
+    return {};
+  }
+  return ROBQueryIDResult{.valid_ = true, .rob_id_ = current_state_.tail_};
+}
+
+ROBQueryResult ROB::Query(uint8_t rob_id)
+{
+  if (current_state_.rob_infos_[rob_id].ready_ &&
+      current_state_.rob_infos_[rob_id].rd_value_valid_)
+  {
+    return ROBQueryResult{
+        .valid_ = true, .value_ = current_state_.rob_infos_[rob_id].rd_value_};
+  }
+  return {};
+}
+
+void ROB::UpdateCurrent() { current_state_ = next_state_; }
+
+void ROB::Execute() // 利用current发送output，以及修改next
+{
+  next_state_ = current_state_;
+  if (current_state_.head_ == current_state_.tail_) // empty
+  {
+    return;
+  }
+  ROBInfo rob_info = current_state_.rob_infos_[current_state_.head_];
+  if (!rob_info.ready_) // 队头没准备好
+  {
+    return;
+  }
+  uint8_t rob_id = current_state_.head_;
+  switch (rob_info.rob_type)
+  {
+  case ROBType::WriteReg:
+  {
+    if (!rob_info.rd_value_valid_)
+    {
+      return;
+    }
+    if (rob_info.rd_valid_)
+    {
+      // To Register
+      register_output_sign_.rd_valid_ = true;
+      register_output_sign_.rd_ = rob_info.rd_;
+      register_output_sign_.value_valid_ = true;
+      register_output_sign_.value_ = rob_info.rd_value_;
+      // To RAT
+      rat_output_sign_.commit_valid_ = true;
+      rat_output_sign_.rd_ = rob_info.rd_;
+      rat_output_sign_.rob_id_ = rob_id;
+    }
+    // 出队
+    next_state_.head_ = (next_state_.head_ + 1) % 16;
+    break;
+  }
+  case ROBType::WriteMem:
+  {
+    if (!rob_info.addr_valid_ || !rob_info.addr_value_valid_ ||
+        !rob_info.write_mem_state_valid_)
+    {
+      return;
+    }
+    if (rob_info.write_mem_state_ ==
+        WriteMemState::NotDone) // 没写完，什么都不能做
+    {
+      return;
+    }
+    lsq_output_sign_.rob_id_valid_ = true;
+    lsq_output_sign_.rob_id_ = rob_id;
+    lsq_output_sign_.write_enabled_ = true;
+    next_state_.rob_infos_[next_state_.head_].write_mem_state_ =
+        WriteMemState::NotDone;
+    break;
+  }
+  case ROBType::Branch:
+  {
+    if (!rob_info.predict_pc_valid_ || !rob_info.real_pc_valid_)
+    {
+      return;
+    }
+    // JAL/JALR 如果需要写 rd，还必须等 PC + 4 返回
+    if (rob_info.rd_valid_ && !rob_info.rd_value_valid_)
+    {
+      return;
+    }
+    if (rob_info.rd_valid_)
+    {
+      // To Register
+      register_output_sign_.rd_valid_ = true;
+      register_output_sign_.rd_ = rob_info.rd_;
+      register_output_sign_.value_valid_ = true;
+      register_output_sign_.value_ = rob_info.rd_value_;
+      // To RAT
+      rat_output_sign_.commit_valid_ = true;
+      rat_output_sign_.rd_ = rob_info.rd_;
+      rat_output_sign_.rob_id_ = rob_id;
+    }
+    if (rob_info.predict_pc_ != rob_info.real_pc_) // 分支预测错误
+    {
+      flush_output_sign_.need_flush_ = true;
+      fetch_flush_output_sign_.need_flush_ = true;
+      fetch_flush_output_sign_.real_pc_valid_ = true;
+      fetch_flush_output_sign_.real_pc_ = rob_info.real_pc_;
+      next_state_ = ROBState{};
+      return;
+    }
+    // 出队
+    next_state_.head_ = (next_state_.head_ + 1) % 16;
+    break;
+  }
+  }
+}
+
+void ROB::UpdateNext() // 利用本周期input计算 next_state_
+{
+  if (flush_output_sign_.need_flush_) // 检测到flush，下周期状态清空
+  {
+    return;
+  }
+  if (lsq_input_sign_.addr_valid_ && lsq_input_sign_.rob_id_valid_ &&
+      lsq_input_sign_.value_valid_)
+  {
+    auto &modify_rob_info = next_state_.rob_infos_[lsq_input_sign_.rob_id_];
+    modify_rob_info.addr_valid_ = true;
+    modify_rob_info.addr_ = lsq_input_sign_.addr_;
+    modify_rob_info.addr_value_valid_ = true;
+    modify_rob_info.addr_value_ = lsq_input_sign_.value_;
+    modify_rob_info.write_mem_state_valid_ = true;
+    modify_rob_info.write_mem_state_ = WriteMemState::NoWrite;
+    modify_rob_info.ready_ = true;
+  }
+  if (memory_input_sign_.is_written_ && memory_input_sign_.rob_id_valid_)
+  {
+    next_state_.head_ = (next_state_.head_ + 1) % 16; // 内存写完出队
+  }
+  if (cdb_input_sign_.rob_id_valid_)
+  {
+    if (cdb_input_sign_.real_pc_valid_)
+    {
+      next_state_.rob_infos_[cdb_input_sign_.rob_id_].real_pc_valid_ = true;
+      next_state_.rob_infos_[cdb_input_sign_.rob_id_].real_pc_ =
+          cdb_input_sign_.real_pc_;
+      next_state_.rob_infos_[cdb_input_sign_.rob_id_].ready_ = true;
+    }
+    if (cdb_input_sign_.value_valid_)
+    {
+      next_state_.rob_infos_[cdb_input_sign_.rob_id_].rd_value_valid_ = true;
+      next_state_.rob_infos_[cdb_input_sign_.rob_id_].rd_value_ =
+          cdb_input_sign_.value_;
+      next_state_.rob_infos_[cdb_input_sign_.rob_id_].ready_ = true;
+    }
+  }
+  if (issue_input_sign_.rob_id_valid_)
+  {
+    switch (issue_input_sign_.rob_type)
+    {
+    case ROBType::WriteReg:
+    {
+      next_state_.rob_infos_[issue_input_sign_.rob_id_] =
+          ROBInfo{.rob_type = ROBType::WriteReg,
+                  .rd_valid_ = issue_input_sign_.rd_valid_,
+                  .rd_ = issue_input_sign_.rd_};
+      next_state_.tail_ = (next_state_.tail_ + 1) % 16; // 入队
+      break;
+    }
+    case ROBType::WriteMem:
+    {
+      next_state_.rob_infos_[issue_input_sign_.rob_id_] =
+          ROBInfo{.rob_type = ROBType::WriteMem,
+                  .write_mem_state_valid_ = true,
+                  .write_mem_state_ = WriteMemState::NoWrite};
+      next_state_.tail_ = (next_state_.tail_ + 1) % 16; // 入队
+      break;
+    }
+    case ROBType::Branch:
+    {
+      next_state_.rob_infos_[issue_input_sign_.rob_id_] =
+          ROBInfo{.rob_type = ROBType::Branch,
+                  .predict_pc_valid_ = issue_input_sign_.predicted_pc_valid_,
+                  .predict_pc_ = issue_input_sign_.predicted_pc_};
+      if (issue_input_sign_.rd_valid_)
+      {
+        next_state_.rob_infos_[issue_input_sign_.rob_id_].rd_valid_ = true;
+        next_state_.rob_infos_[issue_input_sign_.rob_id_].rd_ =
+            issue_input_sign_.rd_;
+      }
+      next_state_.tail_ = (next_state_.tail_ + 1) % 16; // 入队
+      break;
+    }
+    }
+  }
+}
