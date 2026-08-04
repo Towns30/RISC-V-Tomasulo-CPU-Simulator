e2aab0d85849f671b1d7cb64a0fb8baf515afaab
Towns <tangzheyu@sjtu.edu.cn>
2026-08-04 15:51:59 +0800

    feat CPU

---
+++ simulator/include/CPU.hpp
@@ -0,0 +1,75 @@
+#ifndef CPU_HPP
+#define CPU_HPP
+
+#include "ALU.hpp"
+#include "CDB.hpp"
+#include "Decoder.hpp"
+#include "Fetch.hpp"
+#include "Issue.hpp"
+#include "LSQ.hpp"
+#include "Memory.hpp"
+#include "RAT.hpp"
+#include "ROB.hpp"
+#include "RS.hpp"
+#include "Register.hpp"
+#include "Sign.hpp"
+
+class CPU
+{
+public:
+  CPU();
+
+  void Cycle();
+
+  CPU(const CPU &) = delete;
+  CPU &operator=(const CPU &) = delete;
+  CPU(CPU &&) = delete;
+  CPU &operator=(CPU &&) = delete;
+
+private:
+  void SetReadyAll();
+  void UpdateCurrentAll();
+  void ClearSigns();
+  void ExecuteAll();
+  void UpdateNextAll();
+
+  FetchToMemSign fetch_to_memory_sign_{};
+  MemToFetchSign memory_to_fetch_sign_{};
+  FetchToDecoderSign fetch_to_decoder_sign_{};
+  DecoderToIssueSign decoder_to_issue_sign_{};
+
+  IssueToRATSign issue_to_rat_sign_{};
+  IssueToLSQSign issue_to_lsq_sign_{};
+  IssueToRSSign issue_to_rs_sign_{};
+  IssueToROBSign issue_to_rob_sign_{};
+
+  RSToALUSign rs_to_alu_sign_{};
+  ALUToCDBSign alu_to_cdb_sign_{};
+  LSQToCDBSign lsq_to_cdb_sign_{};
+  CDBToRS_LSQ_ROB_IssueSign cdb_broadcast_sign_{};
+
+  LSQToMemSign lsq_to_memory_sign_{};
+  MemToLSQSign memory_to_lsq_sign_{};
+  LSQToROBSign lsq_to_rob_sign_{};
+  ROBToLSQSign rob_to_lsq_sign_{};
+  MemToROBSign memory_to_rob_sign_{};
+
+  ROBToIssue_RAT_LSQ_RS_Decoder_CDB_Memory_ALUFlushSign rob_flush_sign_{};
+  ROBToFetchFlushSign rob_to_fetch_flush_sign_{};
+  ROBToRATSign rob_to_rat_sign_{};
+  ROBToRegisterSign rob_to_register_sign_{};
+
+  Memory memory_;
+  Fetch fetch_;
+  Decoder decoder_;
+  Register register_;
+  RAT rat_;
+  ROB rob_;
+  Issue issue_;
+  RS rs_;
+  ALU alu_;
+  CDB cdb_;
+  LSQ lsq_;
+};
+
+#endif
---
+++ simulator/src/CPU.cpp
@@ -0,0 +1,122 @@
+#include "CPU.hpp"
+
+CPU::CPU()
+    : memory_(fetch_to_memory_sign_, memory_to_fetch_sign_, lsq_to_memory_sign_,
+              memory_to_lsq_sign_, memory_to_rob_sign_, rob_flush_sign_),
+      fetch_(fetch_to_memory_sign_, memory_to_fetch_sign_,
+             fetch_to_decoder_sign_, rob_to_fetch_flush_sign_),
+      decoder_(fetch_to_decoder_sign_, decoder_to_issue_sign_, rob_flush_sign_),
+      register_(rob_to_register_sign_),
+      rat_(issue_to_rat_sign_, rob_to_rat_sign_, rob_flush_sign_),
+      rob_(issue_to_rob_sign_, lsq_to_rob_sign_, cdb_broadcast_sign_,
+           memory_to_rob_sign_, rob_flush_sign_, rob_to_fetch_flush_sign_,
+           rob_to_lsq_sign_, rob_to_rat_sign_, rob_to_register_sign_),
+      issue_(decoder_to_issue_sign_, issue_to_rat_sign_, issue_to_lsq_sign_,
+             issue_to_rs_sign_, issue_to_rob_sign_, cdb_broadcast_sign_,
+             rob_flush_sign_, register_, rat_, rob_),
+      rs_(issue_to_rs_sign_, rs_to_alu_sign_, cdb_broadcast_sign_,
+          rob_flush_sign_),
+      alu_(rs_to_alu_sign_, alu_to_cdb_sign_, rob_flush_sign_),
+      cdb_(alu_to_cdb_sign_, lsq_to_cdb_sign_, cdb_broadcast_sign_,
+           rob_flush_sign_),
+      lsq_(issue_to_lsq_sign_, lsq_to_memory_sign_, lsq_to_cdb_sign_,
+           lsq_to_rob_sign_, cdb_broadcast_sign_, memory_to_lsq_sign_,
+           rob_to_lsq_sign_, rob_flush_sign_)
+{
+}
+
+void CPU::Cycle()
+{
+  UpdateCurrentAll();
+  SetReadyAll();
+  ClearSigns();
+  ExecuteAll();
+  UpdateNextAll();
+}
+
+void CPU::UpdateCurrentAll()
+{
+  memory_.UpdateCurrent();
+  fetch_.UpdateCurrent();
+  decoder_.UpdateCurrent();
+  register_.UpdateCurrent();
+  rat_.UpdateCurrent();
+  rob_.UpdateCurrent();
+  issue_.UpdateCurrent();
+  rs_.UpdateCurrent();
+  alu_.UpdateCurrent();
+  cdb_.UpdateCurrent();
+  lsq_.UpdateCurrent();
+}
+
+void CPU::SetReadyAll()
+{
+  bool ready = false;
+  if (rs_.Ready() && lsq_.Ready() && rob_.Ready())
+  {
+    ready = true;
+  }
+  issue_.SetReady(ready);
+  decoder_.SetReady(ready);
+  fetch_.SetReady(ready);
+  memory_.SetFetchReady(ready);
+}
+
+void CPU::ClearSigns()
+{
+  fetch_to_memory_sign_ = {};
+  memory_to_fetch_sign_ = {};
+  fetch_to_decoder_sign_ = {};
+  decoder_to_issue_sign_ = {};
+
+  issue_to_rat_sign_ = {};
+  issue_to_lsq_sign_ = {};
+  issue_to_rs_sign_ = {};
+  issue_to_rob_sign_ = {};
+
+  rs_to_alu_sign_ = {};
+  alu_to_cdb_sign_ = {};
+  lsq_to_cdb_sign_ = {};
+  cdb_broadcast_sign_ = {};
+
+  lsq_to_memory_sign_ = {};
+  memory_to_lsq_sign_ = {};
+  lsq_to_rob_sign_ = {};
+  rob_to_lsq_sign_ = {};
+  memory_to_rob_sign_ = {};
+
+  rob_flush_sign_ = {};
+  rob_to_fetch_flush_sign_ = {};
+  rob_to_rat_sign_ = {};
+  rob_to_register_sign_ = {};
+}
+
+void CPU::ExecuteAll()
+{
+  rob_.Execute();
+  rs_.Execute();
+  alu_.Execute();
+  lsq_.Execute();
+  memory_.Execute();
+  cdb_.Execute();
+  register_.Execute();
+  rat_.Execute();
+  fetch_.Execute();
+  decoder_.Execute();
+  issue_.Execute();
+}
+
+void CPU::UpdateNextAll()
+{
+  memory_.UpdateNext();
+  fetch_.UpdateNext();
+  decoder_.UpdateNext();
+  register_.UpdateNext();
+  rat_.UpdateNext();
+  rob_.UpdateNext();
+  issue_.UpdateNext();
+  rs_.UpdateNext();
+  alu_.UpdateNext();
+  cdb_.UpdateNext();
+  lsq_.UpdateNext();
+}
